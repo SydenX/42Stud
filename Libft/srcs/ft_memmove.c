e5fd882c31cb586716bf247aab068d92cdbff82b
Jeremy Tollenaers <jtollena@shi-r7-p3.s19.be>
2023-10-10 14:06:05 +0200

    starting memmove

--- Libft/srcs/ft_memmove.c
+++ Libft/srcs/ft_memmove.c
@@ -12,19 +12,19 @@
 
 #include <string.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	const unsigned char	*srccpy;
 	unsigned char	*copy;
 
 	copy = dst;
 	srccpy = src;
-	while (n > 0)
+	while (len > 0)
 	{
 		*copy = *srccpy;
 		srccpy++;
 		copy++;
-		n--;
+		len--;
 	}
 	return (dst);
 }
@@ -32,6 +32,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 #include <stdio.h>
 int	main(int argc, char *argv[])
 {
-	printf("%s", ft_memcpy(argv[1], argv[2], 2));
+	printf("%s", ft_memmove(argv[1], argv[2], 4));
 	return (argc);
 }
